1ab127b50630d7420ebfbcd52bb88f7a5c431a32
serenader <xyslive@gmail.com>
2014-07-21 14:16:28 +0800

    convert the url to the right path. add a feature to handle zip file

--- controller/admin/file.js
+++ controller/admin/file.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var path = require('path');
+var parse = require('url').parse;
+var AdmZip = require('adm-zip');
 var config = require('../../config').config;
 
 var auth_user = require('./index').auth_user;
@@ -10,11 +12,11 @@ var root = config.root_dir + '/upload/';
 
 module.exports = function (router) {
     router.get('/files', auth_user, function (req, res, next) {
-        var url = req.url;
+        var url = parse(req.url);
         var files = [];
         var folders = [];
-        if (req.url.substring(req.url.length-1) === '/') {
-            res.redirect(adminPath+req.url.substring(0, req.url.length-1));
+        if (url.pathname.substring(url.pathname.length-1) === '/') {
+            res.redirect(adminPath+url.pathname.substring(0, url.pathname.length-1));
         }
         fs.stat(root, function (err, stats) {
             if (err && err.code === 'ENOENT') {
@@ -35,19 +37,24 @@ module.exports = function (router) {
                     adminPath: adminPath, 
                     locals: res.locals, 
                     files: files, folders: folders, 
-                    baseLink: url
+                    baseLink: url.pathname
                 });
             });
         });
     });
 
     router.get('/files/*', auth_user, function (req, res, next) {
-        var url = req.url;
-        var pathname = root + decodeURL(url).filePath+decodeURL(url).fileName,
+        var url = parse(req.url);
+        var acturalPath = decodeURIComponent(url.pathname);
+        var pathname = root + decodeURL(acturalPath).filePath+decodeURL(acturalPath).fileName,
             files = [],
             folders = [];
-        if (req.url.substring(req.url.length-1) === '/') {
-            res.redirect(adminPath+req.url.substring(0, req.url.length-1));
+        if (acturalPath.substring(acturalPath.length-1) === '/') {
+            res.redirect(adminPath+acturalPath.substring(0, req.url.length-1));
+        }
+        if (req.query.download === 'direct') {
+            res.download(pathname.split('?download=direct')[0]);
+            return;
         }
         fs.stat(pathname, function (err, stats) {
             if (err || ! stats) {
@@ -73,11 +80,11 @@ module.exports = function (router) {
                         locals: res.locals, 
                         files: files, 
                         folders: folders, 
-                        baseLink: url
+                        baseLink: acturalPath
                     });
                 });
             } else if (stats.isFile()) {
-                var fileName = url.substring(url.lastIndexOf('/')+1);
+                var fileName = acturalPath.substring(acturalPath.lastIndexOf('/')+1);
                 if (/txt|html|css|scss|sass|htm|xml|js|json|md|markdown|jade|php|xml/gi
                     .test(fileName.substring(fileName.lastIndexOf('.')+1))) {    
                     var mode = convertFileExtension(fileName.substring(fileName.lastIndexOf('.')+1));
@@ -86,7 +93,7 @@ module.exports = function (router) {
                             adminPath: adminPath, 
                             locals: res.locals, 
                             file: data, 
-                            link: url, 
+                            link: acturalPath, 
                             mode: mode
                         });
                     });
@@ -98,9 +105,10 @@ module.exports = function (router) {
     });
 
     router.post('/files/rename/*', auth_user, function (req, res, next) {
-        var url = req.url;
-        var fileName = decodeURL(url, 'rename').fileName;
-        var filePath = decodeURL(url, 'rename').filePath;
+        var url = parse(req.url);
+        var acturalPath = decodeURIComponent(url.pathname);
+        var fileName = decodeURL(acturalPath, 'rename').fileName;
+        var filePath = decodeURL(acturalPath, 'rename').filePath;
         var newName = req.body.name;
         fs.rename(root + filePath+fileName,root + filePath+newName,function (err) {
             if (err) {
@@ -112,23 +120,25 @@ module.exports = function (router) {
     });
 
     router.post('/files/edit/*', auth_user, function (req, res, next) {
-        var url = req.url;
+        var url = parse(req.url);
+        var acturalPath = decodeURIComponent(url.pathname);
         var newContent = req.body.file;
-        var pathname = root + decodeURL(url,'edit').filePath + decodeURL(url,'edit').fileName;
+        var pathname = root + decodeURL(acturalPath,'edit').filePath + decodeURL(acturalPath,'edit').fileName;
         fs.writeFile(pathname, newContent, {encoding: 'utf8'}, function (err) {
             if (err) {
                 res.send(err);
                 return false;
             } else {
-                res.redirect(adminPath+'/files/'+url.split('/files/edit/')[1]);
+                res.redirect(adminPath+'/files/'+acturalPath.split('/files/edit/')[1]);
             }
         });
     });
 
     router.post('/files/delete/*', auth_user, function (req, res, next) {
-        var url = req.url;
-        var fileName = decodeURL(url,'delete').fileName;
-        var filePath = decodeURL(url, 'delete').filePath;
+        var url = parse(req.url);
+        var acturalPath = decodeURIComponent(url.pathname);
+        var fileName = decodeURL(acturalPath,'delete').fileName;
+        var filePath = decodeURL(acturalPath, 'delete').filePath;
         var pathname = root + filePath + fileName;
 
         fs.stat(config.root_dir + '/trash', function (err, stats) {
@@ -155,6 +165,19 @@ module.exports = function (router) {
                 res.send('not a folder');
             }
         });
+    });
+
+
+    router.get('/files/unzip/*', auth_user, function (req,res,next) {
+        var url = parse(req.url);
+        var acturalPath = decodeURIComponent(url.pathname);
+        var fileName = decodeURL(acturalPath,'unzip').fileName;
+        var filePath = decodeURL(acturalPath,'unzip').filePath;
+        var pathname = root + filePath + fileName;
+        var zip = new AdmZip(pathname);
+        var zipEntries = zip.getEntries();
+        // TODO handle zip file
+        res.send(zipEntries);
     });
 };
 
