7ce50640ab2a237866358b3806cac3fa6b6dc8b1
serenader <xyslive@gmail.com>
2014-10-25 23:40:45 +0800

    improve file upload module

--- core/server/controller/admin/upload.js
+++ core/server/controller/admin/upload.js
@@ -1,97 +1,54 @@
-var im = require('imagemagick'),
-    formidable = require('formidable'),
-    path = require('path'),
+var path = require('path'),
     Promise = require('bluebird'),
-    fs = Promise.promisifyAll(require('fs')),
-    fsx = Promise.promisifyAll(require('fs-extra')),
+    validator = require('validator'),
+    xss = require('xss'),
     auth_user = require('../../utils/auth_user'),
     log = require('../../utils/log')(),
+    upload = require('../../utils/upload'),
     config = require('../../../../config').config,
     root = config.root_dir,
-    url = config.url,
-    options = {
-        tmpDir: root + '/content/data/tmp',
-        uploadUrl: url.adminUpload,
-        maxPostSize: 11000000000,
-        minFileSize: 1,
-        maxFileSize: 10000000000,
-        acceptFileTypes: /.+/i,
-        imageTypes: /\.(gif|jpe?g|png)$/i,
-        imageVersions: {
-            'thumbnail': {
-                width: 250,
-            }
-        },
-    },
-    nameCountReg = /(?:(?: \(([\d]+)\))?(\.[^.]+))?$/,
-    nameCountFunc = function (s, index, ext) {
-        return ((parseInt(index, 10) || 0) + 1) + (ext || '');
-    },
-    FileInfo = function (file) {
-        this.name = file.name;
-        this.size = file.size;
-        this.type = file.type;
-        this.deleteType = 'DELETE';
-    };
-
-FileInfo.prototype.validate = function (type) {
-    if (options.minFileSize && options.minFileSize > this.size) {
-        this.error = 'File is too small';
-    } else if (options.maxFileSize && options.maxFileSize < this.size) {
-        this.error = 'File is too big';
-    } else if (!type.test(this.name)) {
-        this.error = 'Filetype is not allowed';
-    }
-    return !this.error;
-};
-
-FileInfo.prototype.safeName = function (uploadDir) {
-    this.name = path.basename(this.name).replace(/^\.+/,'').replace(/(|)/, '');
-
-    if (fs.existsSync(uploadDir + '/' + this.name)) {
-        this.name = this.name.replace(nameCountReg, nameCountFunc);    
-    }
-};
-
-FileInfo.prototype.initUrls = function (req, uploadDir, path) {
-    if (! this.error) {
-        var self = this,
-            baseUrl =  path;
-        this.url = this.deleteUrl = baseUrl + '/' + encodeURIComponent(this.name);
-        Object.keys(options.imageVersions).forEach(function (version) {
-
-            if (options.imageTypes.test(self.name)) {
-                self[version + 'Url'] = baseUrl + '/' + version + '/' + encodeURIComponent(self.name);
-            }
-        });
-    }
-};
+    url = config.url;
 
 
 module.exports = function (router) {
     var uploadDir = function (req) {
         return root + '/content/data/public/' + req.session.user.uid + '/upload';
     };
-    router.post(url.adminPostUpload, auth_user, function (req, res, next) {
-        fileUpload(req, res, {
-            uploadDir: uploadDir(req),
-            path: '/static/' + req.session.user.uid + '/upload',
-            typeReg: options.imageTypes
-        }, function (err, files, field) {
-            if (err) {
-                res.json({
-                    status: 0,
-                    error: err
-                });
-            } else {
-                res.json(files);
-            }
+
+    router.post(url.adminUpload + '/*', auth_user, function (req, res, next) {
+        var userName = req.session.user.uid,
+            dir = validator.trim(xss(req.url)).split(url.adminUpload + '/')[1],
+            tmpArr = dir.split('/'),
+            type = tmpArr.shift(),
+            dstDir = tmpArr.join('/'),
+            targetDir = root + '/content/data/' + type + '/' + userName + '/upload/' + dstDir;
+
+        if (type !== 'public' && type !== 'private') {
+            res.json({
+                status: 0,
+                error: 'type error'
+            });
+            return false;
+        }
+
+        upload(req, res, {
+            uploadDir: targetDir,
+            baseUrl: '/static/' + userName + '/upload/' + dstDir,
+            deleteUrl: url.admin + '/' + url.adminUpload + '/' + type + '/' + userName + '/upload/' + dstDir
+        }).then(function (files, field) {
+            res.json(files);
+        }).catch(function (err) {
+            res.json({
+                status: 0,
+                error: err.message
+            });
         });
     });
 
-    router.delete(url.adminPostUpload + '/*', auth_user, function (req, res, next) {
-        var file = path.basename(decodeURIComponent(req.url)),
-            dir = uploadDir(req);
+    router.delete(url.adminUpload + '/*', auth_user, function (req, res, next) {
+        var dir = root + '/content/data/' + validator.trim(xss(req.ur)).split(url.adminUpload)[1],
+            fileName = path.basename(dir);
+
         if (file) {
             fs.unlinkAsync(dir + '/' + file).then(function () {
                 return Object.keys(options.imageVersions).reduce(function (p, version) {
@@ -113,114 +70,3 @@ module.exports = function (router) {
         }
     });
 };
-
-var fileUpload = module.exports.fileUpload = function (req, res, opt, callback) {
-    // opt = {
-    //     uploadDir: 'aaa',
-    //     path: 'aa',
-    //     typeReg: /\.(gif|jpe?g|png)$/i,
-    // }
-    var form = new formidable.IncomingForm(),
-        uploadDir = opt.uploadDir,
-        tmpFiles = [],
-        files = [],
-        map = [],
-        counter = 1,
-        field = {},
-        p,
-        setNoCacheHeaders = function () {
-            res.setHeader('Pragma', 'no-cache');
-            res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-            res.setHeader('Content-Disposition', 'inline; filename="files.json"');
-        };
-
-    form.uploadDir = options.tmpDir;
-
-
-    form.on('fileBegin', function (name, file) {
-
-        var fileinfo = new FileInfo(file);
-        tmpFiles.push(file.path);
-        fileinfo.safeName(uploadDir);
-        map[path.basename(file.path)] = fileinfo;
-        files.push(fileinfo);
-
-    }).on('field', function (name, value) {
-
-        field[name] = value;
-
-    }).on('file', function (name, file) {
-
-        var fileinfo = map[path.basename(file.path)];
-        fileinfo.size = file.size;
-
-        if (! fileinfo.validate(opt.typeReg)) {
-            fs.unlink(file.path);
-            return false;
-        }
-        if (! fs.existsSync(uploadDir)) {
-            fsx.mkdirsSync(uploadDir);
-        }
-        p = fs.renameAsync(file.path, uploadDir + '/' + fileinfo.name).then(function () {
-            if (options.imageTypes.test(fileinfo.name)) {
-                return Object.keys(options.imageVersions).reduce(function (p, version) {
-                    return fsx.mkdirsAsync(uploadDir + '/' + version).then(function () {
-                        var opt = options.imageVersions[version];
-                        return new Promise(function (resolve, reject) {
-                            im.resize({
-                                width: opt.width,
-                                srcPath: uploadDir + '/' + fileinfo.name,
-                                dstPath: uploadDir + '/' + version + '/' + fileinfo.name
-                            }, function (err) {
-                                if (err) {
-                                    reject(err);
-                                } else {
-                                    resolve();
-                                }
-                            });
-                        });
-                    });
-                }, Promise.resolve());
-            }
-        });
-
-    }).on('aborted', function () {
-
-        tmpFiles.forEach(function (file) {
-            fs.unlink(file);
-        });
-
-    }).on('progress', function (bR, bE) {
-
-    }).on('error', function (e) {
-
-        console.error(e);
-
-    }).on('end', function () {
-
-        files.forEach(function (fileinfo) {
-            fileinfo.initUrls(req, opt.uploadDir, opt.path);
-        });
-        p.then(function () {
-            callback(null, files, field);
-        }).catch(function (err) {
-            log.error(err.stack);
-            callback(err.message, files, field);
-        });
-        // files = {
-        //     deleteType: "DELETE",
-        //     deleteUrl: "http://localhost/static/damn/upload/4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg",
-        //     name: "4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg",
-        //     size: 711899,
-        //     type: "image/jpeg",
-        //     url: "http://localhost/static/damn/upload/4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg"
-        // }
-        // field = [
-        //     {
-        //         something: somevalue
-        //     }
-        // ]
-    });
-    form.parse(req);
-};
-module.exports.imageVersions = options.imageVersions;
---
+++ core/server/utils/upload.js
@@ -0,0 +1,181 @@
+module.exports = function (req, res, opt) {
+    var im = require('imagemagick'),
+        formidable = require('formidable'),
+        path = require('path'),
+        Promise = require('bluebird'),
+        fs = Promise.promisifyAll(require('fs')),
+        fsx = Promise.promisifyAll(require('fs-extra')),
+        _ = require('underscore'),
+        auth_user = require('./auth_user'),
+        log = require('./log')(),
+        config = require('../../../config').config,
+        root = config.root_dir,
+        url = config.url,
+        defaultOptions = {
+            tmpDir: root + '/content/data/tmp',
+            uploadDir: root + '/content/data/upload',
+            deleteUrl: '/upload/public/',
+            baseUrl: '/',
+            maxPostSize: 11000000000,
+            minFileSize: 1,
+            maxFileSize: 10000000000,
+            acceptFileTypes: /.+/i,
+            imageTypes: /\.(gif|jpe?g|png)$/i,
+            imageVersions: {
+                'thumbnail': {
+                    width: 250,
+                }
+            },
+        },
+        options = {},
+        nameCountReg = /(?:(?: \(([\d]+)\))?(\.[^.]+))?$/,
+        nameCountFunc = function (s, index, ext) {
+            return ((parseInt(index, 10) || 0) + 1) + (ext || '');
+        },
+        form = new formidable.IncomingForm(),
+        tmpFiles = [],
+        files = [],
+        map = [],
+        field = {},
+        p,
+        setNoCacheHeaders = function () {
+            res.setHeader('Pragma', 'no-cache');
+            res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+            res.setHeader('Content-Disposition', 'inline; filename="files.json"');
+        },
+        FileInfo = function (file) {
+            this.name = file.name;
+            this.size = file.size;
+            this.type = file.type;
+            this.deleteType = 'DELETE';
+        };
+
+    _.extend(options, defaultOptions, opt);
+
+    console.log(options);
+
+    FileInfo.prototype.validate = function () {
+        if (options.minFileSize && options.minFileSize > this.size) {
+            this.error = 'File is too small';
+        } else if (options.maxFileSize && options.maxFileSize < this.size) {
+            this.error = 'File is too big';
+        } else if (!options.acceptFileTypes.test(this.name)) {
+            this.error = 'Filetype is not allowed';
+        }
+        return !this.error;
+    };
+
+    FileInfo.prototype.safeName = function () {
+        this.name = path.basename(this.name).replace(/^\.+/,'').replace(/(|)/, '');
+
+        if (fs.existsSync(options.uploadDir + '/' + this.name)) {
+            this.name = this.name.replace(nameCountReg, nameCountFunc);    
+        }
+    };
+
+    FileInfo.prototype.initUrls = function () {
+        if (! this.error) {
+            var self = this;
+            this.url = options.baseUrl + '/' + encodeURIComponent(this.name);
+            this.deleteUrl = options.deleteUrl + '/' + encodeURIComponent(this.name);
+            Object.keys(options.imageVersions).forEach(function (version) {
+
+                if (options.imageTypes.test(self.name)) {
+                    self[version + 'Url'] = options.baseUrl + '/' + version + '/' + encodeURIComponent(self.name);
+                }
+            });
+        }
+    };
+
+    return new Promise(function (resolve, reject) {
+        form.uploadDir = options.tmpDir;
+        form.on('fileBegin', function (name, file) {
+
+            var fileinfo = new FileInfo(file);
+            tmpFiles.push(file.path);
+            fileinfo.safeName();
+            map[path.basename(file.path)] = fileinfo;
+            files.push(fileinfo);
+
+        }).on('field', function (name, value) {
+
+            field[name] = value;
+
+        }).on('file', function (name, file) {
+
+            var fileinfo = map[path.basename(file.path)];
+            fileinfo.size = file.size;
+
+            if (! fileinfo.validate()) {
+                fs.unlink(file.path);
+                return false;
+            }
+            if (! fs.existsSync(options.uploadDir)) {
+                fsx.mkdirsSync(options.uploadDir);
+            }
+            p = fs.renameAsync(file.path, options.uploadDir + '/' + fileinfo.name).then(function () {
+                if (options.imageTypes.test(fileinfo.name)) {
+                    return Object.keys(options.imageVersions).reduce(function (p, version) {
+                        return fsx.mkdirsAsync(options.uploadDir + '/' + version).then(function () {
+                            var opt = options.imageVersions[version];
+                            return new Promise(function (resolve, reject) {
+                                im.resize({
+                                    width: opt.width,
+                                    srcPath: options.uploadDir + '/' + fileinfo.name,
+                                    dstPath: options.uploadDir + '/' + version + '/' + fileinfo.name
+                                }, function (err) {
+                                    if (err) {
+                                        reject(err);
+                                    } else {
+                                        resolve();
+                                    }
+                                });
+                            });
+                        });
+                    }, Promise.resolve());
+                }
+            });
+
+        }).on('aborted', function () {
+
+            tmpFiles.forEach(function (file) {
+                fs.unlink(file);
+            });
+            reject();
+
+        }).on('progress', function (bR, bE) {
+
+        }).on('error', function (e) {
+
+            log.error(e.stack);
+            reject(e);
+
+        }).on('end', function () {
+
+            files.forEach(function (fileinfo) {
+                fileinfo.initUrls(req, opt.uploadDir, opt.path);
+            });
+            p.then(function () {
+                resolve(files, field);
+            }).catch(function (err) {
+                log.error(err.stack);
+                reject(err);
+            });
+            // files = {
+            //     deleteType: "DELETE",
+            //     deleteUrl: "http://localhost/static/damn/upload/4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg",
+            //     name: "4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg",
+            //     size: 711899,
+            //     type: "image/jpeg",
+            //     url: "http://localhost/static/damn/upload/4fjHtYHdRlSemICxjjBu_IMG_8424(1).jpg"
+            // }
+            // field = [
+            //     {
+            //         something: somevalue
+            //     }
+            // ]
+        });
+        form.parse(req);
+    });
+
+};
