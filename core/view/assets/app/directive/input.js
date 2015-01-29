903b1a9849c679bbb2b49b68a55e29b384a8174c
serenader <xyslive@gmail.com>
2015-01-30 01:28:11 +0800

    improve the md-input directive, now works fine

--- core/view/assets/app/directive/input.js
+++ core/view/assets/app/directive/input.js
@@ -5,6 +5,22 @@
         emailRegExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
 
     angular.module('appModule')
+    .directive('min', function () {
+        return {
+            restrict: 'A',
+            require: 'ngModel',
+            link: function (scope, elements, attrs, ngModel) {
+                var min = attrs.min,
+                    value = ngModel.$modelValue;
+
+                if (value < min) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        };
+    })
     .directive('mdInput', function () {
         return {
             restrict: 'AE',
@@ -15,50 +31,44 @@
                 title: '@',
                 desc: '@',
                 rePwd: '@',
-                value: '='
+                value: '=',
+                updateOn: '@',
             },
             transclude: true,
             require: 'ngModel',
             link: function ($scope, $element, $attrs, ctrl) {
                 var input = $element.find('input');
-                input.val($attrs.value);
+                $scope.$watch(function () {
+                    return ctrl.$modelValue;
+                }, function (value) {
+                    if (value) {
+                        input.val(value);
+                    }
+                });
                 $scope.hasDesc = $attrs.desc;
                 $scope.isFocus = false;
 
-                $scope.$watch('value', function (v) {
-                    input.val(v);
-                    ctrl.$setViewValue(v);
+                input.on('keyup', function () {
+                    if ($scope.updateOn === 'default' || $scope.updateOn !== 'blur') {
+                        $scope.$apply(function () {
+                            ctrl.$setViewValue(input.val());
+                        });
+                    }
+                }).on('blur', function () {
+                    if ($scope.updateOn === 'blur') {
+                        $scope.$apply(function () {
+                            ctrl.$setViewValue(input.val());
+                        });
+                    }
                 });
 
-                input.val($scope.value);
-
                 $scope.hasValue = function () {
-                    return input.val();
+                    return ctrl.$viewValue;
                 };
 
                 $scope.isError = function () {
                     return ctrl.$invalid;
                 };
-
-                input.on('focus', function () {
-                    $scope.isFocus = true;
-                    $scope.$apply();
-                }).on('blur', function () {
-                    $scope.isFocus = false;
-                    $scope.$apply(function () {
-                        ctrl.$setViewValue(input.val());
-                    });
-                }).on('keyup', function () {
-                    $scope.$apply(function () {
-                        ctrl.$setViewValue(input.val());
-                    });
-                }).on('change', function () {
-                    $scope.$apply(function () {
-                        ctrl.$setViewValue(input.val());
-                        $scope.value = input.val();
-                        console.log($scope.value);
-                    });
-                });
 
                 ctrl.$validators.min = function (modelValue, viewValue) {
                     var value = modelValue || viewValue;
@@ -82,7 +92,6 @@
 
                 ctrl.$validators.minLength = function (modelValue, viewValue) {
                     var value = modelValue || viewValue;
-
                     if (!$attrs.minLength || !value) {
                         return true;
                     }
@@ -153,8 +162,6 @@
                 ctrl.$render = function () {
                     input.val(ctrl.$viewVlaue);
                 };
-
-                ctrl.$setViewValue(input.val());
             }
         };
     });
